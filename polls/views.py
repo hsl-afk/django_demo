1e71e5d7c2a1a900fe3b0ca420ab98e74549e610
Harshil kakdiya <hsl@taglineinfotech.com>
2026-06-23 18:03:14 +0530

    testing commit

--- polls/views.py
+++ polls/views.py
@@ -37,7 +37,6 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
         return render(
             request,
             "polls/detail.html",
@@ -59,6 +58,9 @@ def detail(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk= question_id)
     return render(request, "polls/result.html",{"question": question})
+
+def index(request):
+    HttpResponse("polls:result", {})
 
 def index(request): 
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
