21da1ec1271cbb97139b9c313e36fc139a7cf525
Harshil kakdiya <hsl@taglineinfotech.com>
2026-06-24 11:55:18 +0530

    Refactor Question model's was_published_recently method and add unit tests for publication logic

--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question , Choice
 from django.db.models import F
 from django.urls import reverse
-
-from django.db.models import F
 from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
 from django.views import generic
-
+from django.utils import timezone
 from .models import Choice, Question
 
 
@@ -18,9 +13,11 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
-
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 class DetailView(generic.DetailView):
     model = Question
@@ -49,7 +46,7 @@ def vote(request, question_id):
         selected_choice.votes = selected_choice.votes + 1
         selected_choice.save()
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
-# this changes are done to check the merge comnflict and how the merge works using git.
+    
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/details.html", {"question": question})
@@ -58,9 +55,6 @@ def detail(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk= question_id)
     return render(request, "polls/result.html",{"question": question})
-
-def index(request):
-    HttpResponse("polls:result", {})
 
 def index(request): 
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
