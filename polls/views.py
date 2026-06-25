5a4f08a099acfe680cda08c8bad15acf18981bf0
Harshil kakdiya <hsl@taglineinfotech.com>
2026-06-25 12:22:56 +0530

    css with the generated templates

--- polls/views.py
+++ polls/views.py
@@ -21,12 +21,12 @@ class IndexView(generic.ListView):
 
 class DetailView(generic.DetailView):
     model = Question
-    template_name = "polls/detail.html"
+    template_name = "polls/details.html"
 
 
 class ResultsView(generic.DetailView):
     model = Question
-    template_name = "polls/results.html"
+    template_name = "polls/result.html"
 
 
 def vote(request, question_id):
@@ -45,22 +45,6 @@ def vote(request, question_id):
     else:
         selected_choice.votes = selected_choice.votes + 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+        return HttpResponseRedirect(reverse("polls:result", args=(question.id,)))
     
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/details.html", {"question": question})
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk= question_id)
-    return render(request, "polls/result.html",{"question": question})
-
-def index(request): 
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
-
-def add_question(request):
-    return render(request, "polls/add_question.html",{})
     
